9238248535b4b8f438ebb75ed5e1e8e6abd40097
crashh <rad.wozniak@gmail.com>
2013-02-18 23:06:44 +0100

    refactor StocksCtrl

--- app/public/stocks_controller.js
+++ app/public/stocks_controller.js
@@ -2,11 +2,8 @@ function StocksCtrl($scope, Stock, Stocks) {
   $scope.stocks = Stocks.index();
   $scope.predicate = 'status';
 
-  $scope.update = function(stockId,status) {
-    var stock = Stock.get({stockId: stockId}, function(){
-      stock.$update({status:status}, function(){
-        $scope.stocks = Stocks.index();
-      });
-    });
+  $scope.update = function(stock, status){
+    stock.status = status;
+    new Stock(stock).$update({status:status});
   };
 };
